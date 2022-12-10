97744f8bfcbaf8f59a3e05c08ce46bc1ddb7a570
pablo <pablo.garcia.bas@gmail.com>
2022-12-10 11:56:03 +0100

    Update (Pablo) - Added TDA Solver and partida_letras.cpp

---
+++ ej04/estudiante/include/solver.h
@@ -0,0 +1,82 @@
+/**
+ * @file solver.h
+ * @brief Archivo de especificación del TDA Solver
+ * @author Pablo García Bas
+ * @author Andrés Gutiérrez Armenteros
+ * @date Diciembre 2022
+ */
+
+#ifndef P04_JUEGO_LETRAS_SOLVER_H
+#define P04_JUEGO_LETRAS_SOLVER_H
+
+#include "vector"
+#include "dictionary.h"
+#include "letters_set.h"
+#include "algorithm"
+
+/**
+     * @brief TDA Solver
+     *
+     *
+     * Permite obtener la mejor/mejores soluciones (no necesariamente solución única) para una partida y modo de juego determinado.
+     * Está formado por un objeto del TDA Dictionary (posibles soluciones), así como uno del TDA LettersSet (letras disponibles en la partida)
+    */
+class Solver{
+
+private:
+    Dictionary dictionary;
+    LettersSet letters;
+
+    /**
+     * @brief Construye el vector de soluciones (por puntuación) a partir de las letras de una partida.
+     * @param available_letters Vector de letras disponibles para la partida.
+     * @return Par <vector<string>, int>, con el vector de palabras que constituyen las mejores soluciones (puede haber
+     * empates) y la puntuación que obtienen.
+     */
+    pair<vector<string>,int> getSolutionsByScore(const vector<char>& available_letters);
+
+    /**
+     * @brief Construye el vector de soluciones (por longitud) a partir de las letras de una partida.
+     * @param available_letters Vector de letras disponibles para la partida.
+     * @return Par <vector<string>, int>, con el vector de palabras que constituyen las mejores soluciones (puede haber
+     * empates) y la puntuación que obtienen.
+     */
+    pair<vector<string>,int> getSolutionsByLength(const vector<char>& available_letters);
+
+    /**
+     * @brief Construye el vector de soluciones (por puntuación) a partir de las letras de una partida.
+     * @param available_letters Vector de letras disponibles para la partida.
+     * @return Par <vector<string>, int>, con el vector de palabras que constituyen las mejores soluciones (puede haber
+     * empates) y la puntuación que obtienen.
+     */
+
+    /**
+     * @brief Comprueba si una palabra se puede formar con un vector de letras disponibles
+     * @param word Palabra a comprobar.
+     * @param letters Vector de letras disponibles
+     * @return Bool indicando si la palabra se puede formar. True indica que se puede, false indica lo contrario.
+     */
+    bool is_word_possible(string word, vector<char> letters);
+
+public:
+
+    /**
+     * @brief Constructor con parámetros. Crea un Solver que tiene asociado un Dictionary y un LettersSet.
+    */
+    Solver(const Dictionary& dict, const LettersSet& letters_set);
+
+    /**
+     * @brief Construye el vector de soluciones a partir de las letras de una partida.
+     * Dado un conjunto de letras posibles para crear una solución, y el modo de juego con el que se juega la partida,
+     * se construye un vector con las mejores soluciones encontradas en función del tipo de juego planteado.
+     * @param available_letters Vector de letras disponibles para la partida.
+     * @param score_game Bool indicando el tipo de partida. True indica que la partida se juega a puntuación, false que
+     * se juega a longitud.
+     * @return Par <vector<string>, int>, con el vector de palabras que constituyen las mejores soluciones (puede haber
+     * empates) y la puntuación que obtienen.
+     */
+    pair<vector<string>,int> getSolutions(const vector<char>& available_letters, bool score_game);
+
+};
+
+#endif //P04_JUEGO_LETRAS_SOLVER_H
---
+++ ej04/estudiante/src/solver.cpp
@@ -0,0 +1,113 @@
+/**
+ * @file solver.h
+ * @brief Implementación del TDA Solver
+ * @author Pablo García Bas
+ * @author Andrés Gutiérrez Armenteros
+ * @date Diciembre 2022
+ */
+
+#include "solver.h"
+#include "string"
+#include "vector"
+
+// PUBLIC //
+Solver::Solver(const Dictionary& dict, const LettersSet& letters_set){  // Using copy constructors from Dictionary and
+    this->dictionary = dict;                                            // LettersSet
+    this->letters = letters_set;
+}
+
+pair<vector<string>,int> Solver::getSolutions(const vector<char>& available_letters, bool score_game){
+    pair<vector<string>,int> solutions_pair;
+    if(score_game)          // By Score
+        solutions_pair = getSolutionsByScore(available_letters);
+    else                    // By Length
+        solutions_pair = getSolutionsByLength(available_letters);
+    return solutions_pair;
+}
+
+// PRIVATE //
+bool Solver::is_word_possible(string word, vector<char> letters){
+
+    vector<char> letters_copy(letters); // Copy necessary, as "available_letters" is a constant reference
+
+    bool is_possible = true;
+    int i=0;
+
+    if(word.size() > letters_copy.size()) // 1) First check
+        is_possible = false;
+
+    while(is_possible && i<word.size()) { // 2) Second check
+        vector<char>::iterator it = find(letters_copy.begin(), letters_copy.end(), word.at(i));
+        if (it != letters_copy.end()) // 2.1) Word's letter i was found in "letters" vector
+            letters_copy.erase(it);
+        else { // Still, word's letter i could be lowercase and "letters" vector's letters uppercase, and therefore not match
+            it = find(letters_copy.begin(), letters_copy.end(), toupper(word.at(i)));
+            if (it != letters_copy.end())
+                letters_copy.erase(it);
+            else // 2.2) Letter was not found
+                is_possible = false;
+        }
+        i++;
+    }
+    return is_possible;
+}
+
+pair<vector<string>,int> Solver::getSolutionsByScore(const vector<char>& available_letters){
+    int score;
+    pair<vector<string>,int> solutions_pair;
+
+    int i=0;
+
+    for(Dictionary::iterator it=this->dictionary.begin(); it!=this->dictionary.end()/*i<77000*/; ++it){
+
+        i++;
+
+        score = this->letters.getScore(*it); // O(n)
+
+        if (score >= solutions_pair.second){ // Only if word's score is at least as good as the best previous
+                                             // should it be studied if is possible as word
+            if(is_word_possible(*it, available_letters)){ // O(n^2)
+
+                if(score == solutions_pair.second)
+                    solutions_pair.first.push_back(*it);
+
+                else if(score > solutions_pair.second){
+                    solutions_pair.first.clear();
+                    solutions_pair.first.push_back(*it);
+                    solutions_pair.second = score;
+                }
+            }
+        }
+    }
+    return solutions_pair;
+}
+
+pair<vector<string>,int> Solver::getSolutionsByLength(const vector<char>& available_letters){
+    int length;
+    pair<vector<string>,int> solutions_pair;
+
+    int i=0;
+
+    for(Dictionary::iterator it=this->dictionary.begin(); it!=this->dictionary.end()/*i<77000*/; ++it){
+
+        i++;
+
+        length = (*it).size(); // O(n)
+
+        if (length >= solutions_pair.second){   // Only if word's length is at least as good as the best previous
+                                                // should it be studied if is possible as word
+            if(is_word_possible(*it, available_letters)){ // O(n^2)
+
+                if(length == solutions_pair.second)
+                    solutions_pair.first.push_back(*it);
+
+                else if(length > solutions_pair.second){
+                    solutions_pair.first.clear();
+                    solutions_pair.first.push_back(*it);
+                    solutions_pair.second = length;
+                }
+            }
+        }
+    }
+    return solutions_pair;
+}
